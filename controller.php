8d39b7bad1ff7fd1ee4f22cd3d44740a84d61934
Victor Flores <victor.floress@usach.cl>
2012-07-11 12:38:07 -0400

    Mejorando las opciones al agregar lineas y agregar estaciones

--- controller.php
+++ controller.php
@@ -19,5 +19,18 @@
 			}
 			return $matrix;
 		}
+		public static function getAllTermEstations() {
+			$connect = mysql_connect("localhost","metro", "metro123");
+			$db = mysql_select_db("metroSantiago", $connect);
+			
+			$result = mysql_query("SELECT * FROM Primer_anden_linea NATURAL JOIN Anden NATURAL JOIN Estacion");
+			$matrix = NULL;
+			$i = 0;
+			while($fila = mysql_fetch_assoc($result)){
+				$matrix[$i] = $fila;
+				$i = $i + 1;
+			}
+			return $matrix;
+		}
 	}
 ?>
